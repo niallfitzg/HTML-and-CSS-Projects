0cd21ca22f59e0ec8bec3084d3b5b8fbd82139a8
niallfitzg <niallfitzg@gmail.com>
2023-09-19 09:34:18 +0100

    Assignmnet Changes

---
+++ Basic_HTML_and_CSS/VS Code Assignment/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, World';
+console.log(greeting);
+
+var greeting2 = 'Wellcome to Node.js';
+console.log(greeting2);
+
+var sentance1 = 'Learning how to debug code with the debugger';
+console.log(sentanceOne)
