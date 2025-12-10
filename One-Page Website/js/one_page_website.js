a623ada5475631bda9ba10a02838ce6e7cd67650
niallfitzg <niallfitzg@gmail.com>
2025-12-10 09:02:34 +0000

    One-Page Website Submission Assignment
    Functional lightbox added for images in One-Page Website.

---
+++ One-Page Website/js/one_page_website.js
@@ -0,0 +1,41 @@
+// LIGHTBOX FUNCTIONS
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
